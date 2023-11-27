1e48ec7d3fafafd318e33dcbf3249bed4dbaf338
Kartik Bambhaniya <kh.developer.01@gmail.com>
2023-11-27 17:01:59 +0530

    Uploading Files

---
+++ code.js
@@ -0,0 +1,10 @@
+"use strict";
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { themeColors: true, width: 600, height: 720 });
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+};
